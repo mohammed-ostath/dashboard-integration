bc92b50ecb7bde844b5cda8059b9f2dedcfbba49
mohammed-ostath <mohammedostath@gmail.com>
2023-09-10 17:08:53 +0300

    dashboard

---
+++ resources/views/admin/layouts/scripts.blade.php
@@ -0,0 +1,16 @@
+   <!-- REQUIRED SCRIPTS -->
+
+   <!-- jQuery -->
+   <script src="{{ asset('adminassets/plugins/jquery/jquery.min.js') }}"></script>
+   <!-- Bootstrap -->
+   <script src="{{ asset('adminassets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+   <!-- AdminLTE -->
+   <script src="{{ asset('adminassets/dist/js/adminlte.js') }}"></script>
+
+   <!-- OPTIONAL SCRIPTS -->
+   <script src="{{ asset('adminassets/plugins/chart.js/Chart.min.js') }}"></script>
+   <!-- AdminLTE for demo purposes -->
+   <script src="{{ asset('adminassets/dist/js/demo.js') }}"></script>
+   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+   <script src="{{ asset('adminassets/dist/js/pages/dashboard3.js') }}"></script>
+   @yield('scripts')
